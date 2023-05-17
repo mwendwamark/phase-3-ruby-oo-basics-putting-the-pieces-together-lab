8ba8a0e741ca708729c1b0aee442078bd3ed6e45
mark mwendwa <mwendwamark310@gmail.com>
2023-05-17 13:18:07 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
 
+    def initialize(title = "And Then There Were None" )
+        @title= title        
+    end
+
+    def title=(value)
+        @title= value
+    end
+
+    def title
+        @title
+    end
+
+    def turn_page 
+        puts ("Flipping the page...wow, you read fast!")
+    end
+end
 
